52429b298a01922ca7f61ad15da3c7ab6315fbcc
webermaximilian <maximilian.weber@student.unisg.ch>
2018-11-29 09:06:43 +0100

    cleaned yahoo classifier.py

--- text_classification/1.Yahoo_Classifier.py
+++ text_classification/1.Yahoo_Classifier.py
@@ -3,70 +3,41 @@
 
 # In[74]:
 
-
+#pandas for reading csv
+#numpy for mathematical calculations
 import pandas as pd, numpy as np
-#regular expression
+#regular expression - manipulate sentences
 import re
 
+#Hyperparameters
+#MAX_NB_WORDS provides limit of words stored in vocabulary
 MAX_NB_WORDS = 200000
+#MAX_SEQUENCE_LENGTH sets limit of words in sequence
 MAX_SEQUENCE_LENGTH = 150
 
 
 # In[75]:
 
 
-#Load whole yahoo training dataset
+#Load whole yahoo TRAINING dataset from filepath
 unprocessed_training_data = pd.read_csv(r'C:\Users\maximilian.weber\OneDrive - Synpulse\UserRoaming\Desktop\TextClassificationDatasets-20181112T073934Z-001\TextClassificationDatasets\yahoo_answers_csv\train.csv', header=None)
+#own header titles
 unprocessed_training_data.columns = ['labels','questions_1','questions_2', 'features']
 
 
 # In[76]:
 
 
-#Load whole yahoo testing dataset
+#Load whole yahoo TESTING dataset from filepath
 unprocessed_testing_data = pd.read_csv(r'C:\Users\maximilian.weber\OneDrive - Synpulse\UserRoaming\Desktop\TextClassificationDatasets-20181112T073934Z-001\TextClassificationDatasets\yahoo_answers_csv\test.csv', header=None)
+#Own header titles
 unprocessed_testing_data.columns = ['labels','questions_1','questions_2', 'features']
-
-
-# In[77]:
-
-
-unprocessed_training_data.head()
-
-
-# In[78]:
-
-
-unprocessed_testing_data.head()
-
-
-# In[79]:
-
-
-print(len(unprocessed_training_data))
-
-
-# In[80]:
-
-
-print(len(unprocessed_testing_data))
-
-
-# In[81]:
-
-
-unprocessed_training_data.isnull().sum()
-
-
-# In[82]:
-
-
-unprocessed_testing_data.isnull().sum()
 
 
 # In[83]:
 
 
+#delete unnecessary columns of TRAINING dataset
 del unprocessed_training_data['questions_1']
 del unprocessed_training_data['questions_2']
 
@@ -74,52 +45,29 @@ del unprocessed_training_data['questions_2']
 # In[84]:
 
 
+#delete unnecessary columns of TESTING dataset
 del unprocessed_testing_data['questions_1']
 del unprocessed_testing_data['questions_2']
-
-
-# In[85]:
-
-
-unprocessed_training_data.head()
-
-
-# In[86]:
-
-
-unprocessed_testing_data.head()
 
 
 # In[87]:
 
 
+#delete all rows without content of TRAINING dataset
 processed_training_data = unprocessed_training_data.dropna()
-processed_training_data.isnull().sum()
 
 
 # In[88]:
 
 
+#delete all rows without content of TESTING dataset
 processed_testing_data = unprocessed_testing_data.dropna()
-processed_testing_data.isnull().sum()
-
-
-# In[89]:
-
-
-print(len(processed_training_data))
-
-
-# In[90]:
-
-
-print(len(processed_testing_data))
 
 
 # In[91]:
 
 
-#Get data content of training data with label 1-3
+#Get data content of TRAINING data with label 1-3
 training_data_label_1 = processed_training_data[processed_training_data["labels"] == 1]
 training_data_label_2 = processed_training_data[processed_training_data["labels"] == 2]
 training_data_label_3 = processed_training_data[processed_training_data["labels"] == 3]
@@ -128,204 +76,132 @@ training_data_label_3 = processed_training_data[processed_training_data["labels"
 # In[92]:
 
 
-##Get data content of testing data with label 1-3
+##Get data content of TESTING data with label 1-3
 testing_data_label_1 = processed_testing_data[processed_testing_data['labels'] == 1]
 testing_data_label_2 = processed_testing_data[processed_testing_data['labels'] == 2]
 testing_data_label_3 = processed_testing_data[processed_testing_data['labels'] == 3]
 
 
-# In[93]:
-
-
-print(len(training_data_label_1))
-print(len(training_data_label_2))
-print(len(training_data_label_3))
-
-
-# In[94]:
-
-
-print(len(testing_data_label_1))
-print(len(testing_data_label_2))
-print(len(testing_data_label_3))
-
-
 # In[95]:
 
 
-#cut datasets with label 1-3 to size of 10'000
+#cut ONLY TRAINING datasets with label 1-3 to size of 50'000
 training_data_label_1 = training_data_label_1[:50000]
 training_data_label_2 = training_data_label_2[:50000]
 training_data_label_3 = training_data_label_3[:50000]
 
 
-# In[96]:
-
-
-print(len(training_data_label_1))
-print(len(training_data_label_2))
-print(len(training_data_label_3))
-
-
-# In[97]:
-
-
-training_data_label_1.head()
-
-
 # In[98]:
 
 
-#merge training training data label 1 with 2
+#merge TRAINING data label 1 with 2
 training_data_label_1_2 = training_data_label_1.append(training_data_label_2)
-
-
-# In[99]:
-
-
 #merge training data label 1 & 2 with 3
 training_data_label_1_2_3 = training_data_label_1_2.append(training_data_label_3)
-
-
-# In[100]:
-
-
-print(len(training_data_label_1_2_3))
-
-
-# In[101]:
-
-
-testing_data_label_1.head()
 
 
 # In[102]:
 
 
-#merge testinig label data
+#merge TESTING data label 1 with 2
 testing_data_label_1_2 = testing_data_label_1.append(testing_data_label_2)
-
-
-# In[103]:
-
-
+#merge TESTING data label 1 & 2 with 3
 testing_data_label_1_2_3 = testing_data_label_1_2.append(testing_data_label_3)
-
-
-# In[104]:
-
-
-print(len(testing_data_label_1_2_3))
 
 
 # In[105]:
 
 
-#shuffle training data
+#shuffle TRAINING data
 from sklearn.utils import shuffle
 shuffled_training_data = shuffle(training_data_label_1_2_3)
-
-
-# In[106]:
-
-
-#shuffle testing data
+#shuffle TESTING data
 shuffled_testing_data = shuffle(testing_data_label_1_2_3)
 
 
 # In[107]:
 
 
-#fill x_train with training sentences
-#fill y_train with corresponding labels
+#unordered because data was removed & appended --> some n/a arrays
+#fill x_train_unordered with TRAINING sentences
+#fill y_train_unordered with corresponding labels
 x_train_unordered = shuffled_training_data['features']
 y_train_unordered = shuffled_training_data['labels']
-
-
-# In[108]:
-
-
-print(len(x_train_unordered))
-print(len(y_train_unordered))
 
 
 # In[109]:
 
 
-#fill x_test with testing sentences
-#fill y_test with corresponding labels (validation purpose)
+#unordered because data was removed & appended --> some n/a arrays
+#fill x_test_unordered with testing sentences
+#fill y_test_unordered with corresponding labels (validation purpose ONLY)
 x_test_unordered = shuffled_testing_data['features']
 y_test_unordered = shuffled_testing_data['labels']
-
-
-# In[110]:
-
-
-print(len(x_test_unordered))
-print(len(y_test_unordered))
 
 
 # In[111]:
 
 
+#removes n/a arrays in x_train_unordered and y_train_unordered
 x_train = []
 y_train = []
 for i in y_train_unordered:
     y_train.append(i)
-    
+
 for i in x_train_unordered:
     x_train.append(str(i))
 
-y_train = np.array(y_train).reshape(-1,1) #important for keras
+#important for keras model (matrix format)
+y_train = np.array(y_train).reshape(-1,1)
 
 
 # In[112]:
 
 
+#removes n/a arrays in x_test_unordered and y_test_unordered
 x_test = []
 y_test = []
 for i in y_test_unordered:
     y_test.append(i)
-    
+
 for i in x_test_unordered:
     x_test.append(i)
+#no need for reshaping because nothing gets trained in keras model
 
 
 # In[113]:
 
 
+#WordNetLemmatizer formats words back to its basis - removes noise in dataset
+#example: go, went, gone, goes, going --> go
 import nltk
 from nltk.stem import WordNetLemmatizer
+#stopwords removes any unvaluable words like this, for, in, ...
+from nltk.corpus import stopwords
 nltk.download('wordnet')
 
 
 # In[114]:
 
-
-from nltk.corpus import stopwords
-
+@Pre raw sentences as Args
+@Post cleaned wordlist
 #convert into a list of words
 #remove unnecessary split into words, no hyphens
 def sentence_to_wordlist(raw):
     """
-    Receives a raw review and clean it using the following steps:
-    1. Remove all non-words
-    2. Transform the review in lower case
-    3. Remove all stop words
-    4. Perform stemming
-
-    Args:
-        review: the review that iwill be cleaned
-    Returns:
-        a clean review using the mentioned steps above.
+    Receives a raw sentence and cleans it using the following steps:
+    1. Includes only words and numbers
+    2. Transforms the review in lower case
+    3. Removes all stop words
+    4. Performs lemmatize
     """
-    
+
     clean = re.sub("[^A-Za-z0-9]", " ", str(raw))
     clean = clean.lower()
     review = re.compile(r'\b(' + r'|'.join(stopwords.words('english')) + r')\b\s*')
     clean = review.sub('', str(clean))
     clean = clean.split()
-    
+
     lemmatizer = WordNetLemmatizer()
     clean = [lemmatizer.lemmatize(i, pos='v') for i in clean]
     return clean
@@ -334,33 +210,21 @@ def sentence_to_wordlist(raw):
 # In[43]:
 
 
+#displays loading bar
 from tqdm import tqdm
-
-#split trainig sentences into words
+#split TRAINING sentences into wordlist
 split_training_sentences = []
 for raw_sentence in tqdm(x_train):
     split_training_sentences.append(sentence_to_wordlist(raw_sentence))
 
 
-# In[44]:
-
-
-print(split_training_sentences[132])
-
-
 # In[45]:
 
 
-#split testing sententes into words
+#split TESTING sententes into wordlist
 split_testing_sentences = []
 for raw in tqdm(x_test):
     split_testing_sentences.append(sentence_to_wordlist(raw))
-
-
-# In[46]:
-
-
-print(split_testing_sentences[4])
 
 
 # In[72]:
@@ -369,7 +233,7 @@ print(split_testing_sentences[4])
 #text to numbers ordered by most used 1 to less used high number
 from keras.preprocessing.text import Tokenizer
 
-#allows to vectorize a text corpus, by turning each text into either a sequence of integers
+#allows to vectorize a text corpus, by turning each text into a sequence of integers
 tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
 tokenizer.fit_on_texts(split_training_sentences)
 
@@ -382,8 +246,7 @@ vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 ind
 
 
 import pickle
-
-# saving tokenizer
+#saves tokenizer
 with open('tokenizer.pickle', 'wb') as handle:
     pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -391,24 +254,18 @@ with open('tokenizer.pickle', 'wb') as handle:
 # In[48]:
 
 
-#pad every sentence to the same sentence length
-from keras.preprocessing.sequence import pad_sequences 
-
+#pad every sentence to the same sentence length (necessary for matrix calculations)
+from keras.preprocessing.sequence import pad_sequences
 maxlen = MAX_SEQUENCE_LENGTH
-
 padded_training_set = pad_sequences(tokenized_training_set, maxlen=maxlen)
 padded_testing_set = pad_sequences(tokenized_testing_set, maxlen=maxlen)
 
 
-# In[49]:
-
-
-print(padded_training_set[4])
-
-
 # In[50]:
 
-
+@Pre filepath of pretrained model, word_index of pretrained tokenizer, embedding_dim as Hyperparameter
+@Post returns embedding_matrix which is used in keras model
+#creates embedding matrix for later use with pretrained glove model
 def create_embedding_matrix(filepath, word_index, embedding_dim):
     vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
     embedding_matrix = np.zeros((vocab_size, embedding_dim))
@@ -417,7 +274,7 @@ def create_embedding_matrix(filepath, word_index, embedding_dim):
         for line in f:
             word, *vector = line.split()
             if word in word_index:
-                idx = word_index[word] 
+                idx = word_index[word]
                 embedding_matrix[idx] = np.array(
                     vector, dtype=np.float32)[:embedding_dim]
 
@@ -427,6 +284,7 @@ def create_embedding_matrix(filepath, word_index, embedding_dim):
 # In[51]:
 
 
+#builds embedding_matrix with pretrained glove model
 embedding_dim = 50
 embedding_matrix = create_embedding_matrix(
     r'C:\Users\maximilian.weber\Downloads\glove.6B\glove.6B.50d.txt',
@@ -436,27 +294,24 @@ embedding_matrix = create_embedding_matrix(
 # In[52]:
 
 
-#necessary if not binary
+#necessary if model classifies non-binary
 from keras.utils.np_utils import to_categorical
-
 categorical_labels = to_categorical(y_train)
 
 
 # In[53]:
 
 
+#keras model
 from keras.models import Sequential
 from keras import layers
-
-embedding_dim = 50
-
+#embedding_dim = 50 --> unnecessary predefined above
 model = Sequential()
-model.add(layers.Embedding(input_dim=vocab_size, 
+model.add(layers.Embedding(input_dim=vocab_size,
                            output_dim=embedding_dim,
                            weights=[embedding_matrix],
-                           input_length=maxlen, 
+                           input_length=maxlen,
                            trainable=True))
-
 model.add(layers.SpatialDropout1D(0.5))
 model.add(layers.LSTM(196, dropout=0.2, recurrent_dropout=0.2))
 model.add(layers.Dense(4, activation='softmax'))
@@ -483,7 +338,6 @@ results = model.predict(padded_testing_set)
 
 # Save the weights
 model.save_weights('model_weights.h5')
-
 # Save the model architecture
 with open('model_architecture.json', 'w') as f:
     f.write(model.to_json())
@@ -492,6 +346,7 @@ with open('model_architecture.json', 'w') as f:
 # In[67]:
 
 
+#TESTING
 number = 501
 
 probability = max(results[number])
@@ -520,107 +375,10 @@ print(100*'_')
 print(x_test[number])
 
 
-# In[57]:
-
-
-#gets all maximum numbers of 3 predictions per array
-new_list = []
-for i in results:
-    new_list.append(max(i))
-
-
-# In[58]:
-
-
-#gets all categories corresponding to each array
-categories_list = []
-for i in results:
-    categories_list.append(np.argmax(i))
-
-
-# In[115]:
-
-
-#amount of overconfident predictions
-overconfident_amount = 0
-for i, j, k in zip(new_list, y_test, categories_list):
-    if i >= 0.9 and j != k:
-        overconfident_amount = overconfident_amount + 1
-    else:
-        pass
-
-
-# In[116]:
-
-
-#go through all overconfident predicted ones and give back index in list
-lookagain = []
-for i, j, k in zip(new_list, y_test, categories_list):
-    if i >= 0.9 and j != k:
-        lookagain.append(new_list.index(i))
-    else:
-        pass
-
-
-# In[117]:
-
-
-print(lookagain[449])
-
-
-# In[109]:
-
-
-##NOTWORKING FOR NOW!!!!
-listofallwrong = []
-number = 0
-for i in split_testing_sentences:
-    if split_testing_sentences.index(i) == lookagain[number]:
-        listofallwrong.append(i)
-        number = number + 1
-    else:
-        pass
-
-
-# In[118]:
-
-
-percent = round(overconfident_amount/len(results) * 100,2)
-print(str(percent)+'%')
-print(overconfident_amount)
-print(len(results))
-
-#vorher 3% jetzt 1.56% mit glove trainable = false
-
-
-# In[375]:
-
-
-good = 0
-bad = 0
-for x, y in zip(results, y_test):
-    true_category = y
-    probability = max(x)
-    predicted_category = np.argmax(x)
-
-    if predicted_category == true_category:
-        good = good + 1
-    else:
-        bad = bad + 1
-
-
-# In[376]:
-
-
-print(good)
-print(bad)
-ow = bad + good
-print(str(round(good/ow*100))+'%')
-
-
 # In[61]:
 
 
+#model predictor
 example = input('Test: ')
 example = sentence_to_wordlist(example)
 example = tokenizer.texts_to_sequences([example])
@@ -637,4 +395,3 @@ elif number == 2:
     print('Science & Math')
 elif number == 3:
     print('Health')
-
