67412d6a28feb4b607c374323550df309acd95e7
webermaximilian <maximilian.weber@student.unisg.ch>
2018-12-03 17:42:24 +0100

    object oriented

--- text_classification/function_library.py
+++ text_classification/function_library.py
@@ -39,14 +39,7 @@ def delete_data(unprocessed_training_data, unprocessed_testing_data):
     return processed_training_data, processed_testing_data
 
 
-def drop_na_data():
-    #delete all rows without content of TRAINING dataset
-    processed_training_data = unprocessed_training_data.dropna()
-    #delete all rows without content of TESTING dataset
-    processed_testing_data = unprocessed_testing_data.dropna()
-
-
-def get_specific_content():
+def get_specific_content(processed_training_data, processed_testing_data):
     #Get data content of TRAINING data with label 1-3
     training_data_label_1 = processed_training_data[processed_training_data["labels"] == 1]
     training_data_label_2 = processed_training_data[processed_training_data["labels"] == 2]
@@ -55,16 +48,18 @@ def get_specific_content():
     testing_data_label_1 = processed_testing_data[processed_testing_data['labels'] == 1]
     testing_data_label_2 = processed_testing_data[processed_testing_data['labels'] == 2]
     testing_data_label_3 = processed_testing_data[processed_testing_data['labels'] == 3]
+    return training_data_label_1, training_data_label_2, training_data_label_3, testing_data_label_1, testing_data_label_2, testing_data_label_3
 
 
-def cut_training_data():
+def cut_training_data(training_data_label_1, training_data_label_2, training_data_label_3):
     #cut ONLY TRAINING datasets with label 1-3 to size of 50'000
     training_data_label_1 = training_data_label_1[:50000]
     training_data_label_2 = training_data_label_2[:50000]
     training_data_label_3 = training_data_label_3[:50000]
+    return training_data_label_1, training_data_label_2, training_data_label_3
 
 
-def merge_data():
+def merge_data(training_data_label_1, training_data_label_2, training_data_label_3, testing_data_label_1, testing_data_label_2, testing_data_label_3):
     #merge TRAINING data label 1 with 2
     training_data_label_1_2 = training_data_label_1.append(training_data_label_2)
     #merge TRAINING data label 1 & 2 with 3
@@ -73,17 +68,19 @@ def merge_data():
     testing_data_label_1_2 = testing_data_label_1.append(testing_data_label_2)
     #merge TESTING data label 1 & 2 with 3
     testing_data_label_1_2_3 = testing_data_label_1_2.append(testing_data_label_3)
+    return training_data_label_1_2_3, testing_data_label_1_2_3
 
 
-def shuffle_data():
+def shuffle_data(training_data_label_1_2_3, testing_data_label_1_2_3):
     #shuffle TRAINING data
     from sklearn.utils import shuffle
     shuffled_training_data = shuffle(training_data_label_1_2_3)
     #shuffle TESTING data
     shuffled_testing_data = shuffle(testing_data_label_1_2_3)
+    return shuffled_training_data, shuffled_testing_data
 
 
-def get_features_and_labels():
+def get_features_and_labels(shuffled_training_data, shuffled_testing_data):
     #unordered because data was removed & appended --> some n/a arrays
     #fill x_train_unordered with TRAINING sentences
     #fill y_train_unordered with corresponding labels
@@ -94,9 +91,10 @@ def get_features_and_labels():
     #fill y_test_unordered with corresponding labels (validation purpose ONLY)
     x_test_unordered = shuffled_testing_data['features']
     y_test_unordered = shuffled_testing_data['labels']
+    return x_train_unordered, y_train_unordered, x_test_unordered, y_test_unordered
 
 
-def remove_na_cells():
+def remove_na_cells(x_train_unordered, y_train_unordered, x_test_unordered, y_test_unordered):
     #removes n/a arrays in x_train_unordered and y_train_unordered
     x_train = []
     y_train = []
@@ -117,13 +115,14 @@ def remove_na_cells():
     for i in x_test_unordered:
         x_test.append(i)
     #no need for reshaping because nothing gets trained in keras model
+    return x_train, y_train, x_test, y_test
 
 
 def load_lemmatizer():
     #WordNetLemmatizer formats words back to its basis - removes noise in dataset
     #example: go, went, gone, goes, going --> go
     import nltk
-    from nltk.stem import WordNetLemmatizers
+    from nltk.stem import WordNetLemmatizer
     nltk.download('wordnet')
 
 
@@ -133,8 +132,8 @@ def load_lemmatizer():
 #remove unnecessary split into words, no hyphens
 def sentence_to_wordlist(raw):
     import nltk
-    from nltk.stem import WordNetLemmatizer
     #stopwords removes any unvaluable words like this, for, in, ...
+    from nltk.stem import WordNetLemmatizer
     from nltk.corpus import stopwords
     """
     Receives a raw sentence and cleans it using the following steps:
@@ -155,7 +154,7 @@ def sentence_to_wordlist(raw):
     return clean
 
 
-def process_sentences_to_wordlist():
+def process_sentences_to_wordlist(x_train, x_test):
     #displays loading bar
     from tqdm import tqdm
     #split TRAINING sentences into wordlist
@@ -167,9 +166,10 @@ def process_sentences_to_wordlist():
     split_testing_sentences = []
     for raw in tqdm(x_test):
         split_testing_sentences.append(sentence_to_wordlist(raw))
+    return split_training_sentences, split_testing_sentences
 
 
-def perform_tokenizer():
+def perform_tokenizer(split_training_sentences, split_testing_sentences, MAX_NB_WORDS):
     #text to numbers ordered by most used 1 to less used high number
     from keras.preprocessing.text import Tokenizer
 
@@ -180,20 +180,74 @@ def perform_tokenizer():
     tokenized_training_set = tokenizer.texts_to_sequences(split_training_sentences)
     tokenized_testing_set = tokenizer.texts_to_sequences(split_testing_sentences)
     vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
+    return tokenizer, tokenized_training_set, tokenized_testing_set, vocab_size
 
-def save_tokenizer():
+def save_tokenizer(tokenizer):
     import pickle
     #saves tokenizer
     with open('tokenizer.pickle', 'wb') as handle:
         pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-#had to take it out of the function because model_predictor needed it!!!!
-from keras.preprocessing.sequence import pad_sequences
-maxlen = MAX_SEQUENCE_LENGTH
-def pad_tokenized_sequences():
+
+def save_vocab_size(vocab_size):
+    import pickle
+    #saves tokenizer
+    with open('vocab_size.pickle', 'wb') as handle:
+        pickle.dump(vocab_size, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def pad_tokenized_sequences(tokenized_training_set, tokenized_testing_set, MAX_SEQUENCE_LENGTH):
+    from keras.preprocessing.sequence import pad_sequences
     #pad every sentence to the same sentence length (necessary for matrix calculations)
-    padded_training_set = pad_sequences(tokenized_training_set, maxlen=maxlen)
-    padded_testing_set = pad_sequences(tokenized_testing_set, maxlen=maxlen)
+    padded_training_set = pad_sequences(tokenized_training_set, maxlen=MAX_SEQUENCE_LENGTH)
+    padded_testing_set = pad_sequences(tokenized_testing_set, maxlen=MAX_SEQUENCE_LENGTH)
+    return padded_training_set, padded_testing_set
+
+
+def save_preprocessed_train_data(padded_training_set):
+    import pickle
+    #save train data
+    with open('processed_training_data.pickle', 'wb') as handle:
+        pickle.dump(padded_training_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def save_preprocessed_test_data(padded_testing_set):
+    import pickle
+    #save test data
+    with open('processed_testing_data.pickle', 'wb') as handle:
+        pickle.dump(padded_testing_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_tokenizer():
+    import pickle
+    # loading tokenizer
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    return tokenizer
+
+
+def load_vocab_size():
+    import pickle
+    # loading tokenizer
+    with open('vocab_size.pickle', 'rb') as handle:
+        vocab_size = pickle.load(handle)
+    return vocab_size
+
+
+def load_preprocessed_train_data():
+    import pickle
+    # loading train data
+    with open('processed_training_data.pickle', 'rb') as handle:
+        padded_training_set = pickle.load(handle)
+    return padded_training_set
+
+
+def load_preprocessed_test_data():
+    import pickle
+    # loading test data
+    with open('processed_testing_data.pickle', 'rb') as handle:
+        padded_testing_set = pickle.load(handle)
+    return padded_testing_set
 
 
 #@Pre filepath of pretrained model, word_index of pretrained tokenizer, embedding_dim as Hyperparameter
@@ -214,14 +268,15 @@ def create_embedding_matrix(filepath, word_index, embedding_dim):
     return embedding_matrix
 
 
-def build_embedding_matrix():
+def build_embedding_matrix(path_to_origin, tokenizer):
     #builds embedding_matrix with pretrained glove model
     embedding_matrix = create_embedding_matrix(
-        r'C:\Users\maximilian.weber\Downloads\glove.6B\glove.6B.50d.txt',
+        path_to_origin + '/glove.6B/glove.6B.50d.txt',
         tokenizer.word_index, embedding_dim)
 
 
-def predefined_keras_transformation():
+def predefined_keras_transformation(y_train):
     #necessary if model classifies non-binary
     from keras.utils.np_utils import to_categorical
     categorical_labels = to_categorical(y_train)
+    return categorical_labels
